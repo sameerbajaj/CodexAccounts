3a54a7f4e6a266006dac218fe7b7cde28625e66f
sameerbajaj <sameerbajaj24@gmail.com>
2026-02-21 09:50:43 -0800

    Initial Commit

---
+++ CodexAccounts/CodexAccountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexAccountsApp.swift
+//  CodexAccounts
+//
+//  Created by Sameer Bajaj on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexAccountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodexAccountsTests/CodexAccountsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodexAccountsTests.swift
+//  CodexAccountsTests
+//
+//  Created by Sameer Bajaj on 2/21/26.
+//
+
+import Testing
+@testable import CodexAccounts
+
+struct CodexAccountsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
